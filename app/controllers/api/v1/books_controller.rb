c16307b4497163ae01234aa0a6eac2d78f1a7863
Ihor <ihor@ignars-mbp.home>
2024-03-14 14:34:13 -0400

    MY first super late commit API

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class BooksController < ApplicationController
+      def index
+        render json: Book.all
+      end
+
+      def create
+        book = Book.new(book_params)
+
+        if book.save
+          render json: book, status: :created
+        else
+          render json: book.errors, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        @book = Book.find(params[:id])
+        render json: @book
+      end
+
+      def destroy
+        Book.find(params[:id]).destroy!
+
+        head :no_content
+      end
+
+      private
+
+      def book_params
+        params.require(:book).permit(:title, :author)
+      end
+    end
+  end
+end
